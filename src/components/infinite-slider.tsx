b778ee0f1d4e331541d7828f98c15c137a4cee8a
LuaAguirre <renatoag98@gmail.com>
2024-11-15 00:56:06 -0500

    feature: update icons in slider

--- src/components/infinite-slider.tsx
+++ src/components/infinite-slider.tsx
@@ -1,40 +1,39 @@
-import JavaScript from "./slider/javascript";
 import TypeScript from "./slider/typescript";
-import HTML5 from "./slider/html";
 import React from "./slider/react";
-import CSS from "./slider/css";
 import TailwindCSS from "./slider/tailwindcss";
 import Nextjs from "./slider/nextjs";
 import Astro from "./slider/astro";
 import PostgreSQL from "./slider/postgresql";
 import Shadcnui from "./slider/shadcnui";
-import Git from "./slider/git";
-import Github from "./slider/github";
 import Playwright from "./slider/playwright";
-import Vercel from "./slider/vercel";
 import Angular from "./slider/angular";
+import Vue from "./slider/vue";
+import Laravel from "./slider/laravel";
+import Jest from "./slider/jest";
+import MySQL from "./slider/mysql";
+import Docker from "./slider/docker";
+import Expressjs from "./slider/expressjs";
 
-// Define el tipo para los elementos de LOGOS
 interface Logo {
   component: JSX.Element;
   name: string;
 }
 
 const LOGOS: Logo[] = [
-  { component: <HTML5 width={24} height={24} />, name: "HTML5" },
-  { component: <CSS width={24} height={24} />, name: "CSS" },
-  { component: <JavaScript width={24} height={24} />, name: "JavaScript" },
-  { component: <React width={24} height={24} />, name: "React" },
   { component: <TypeScript width={24} height={24} />, name: "TypeScript" },
+  { component: <React width={24} height={24} />, name: "React" },
   { component: <Angular width={24} height={24} />, name: "Angular" },
+  { component: <Vue width={24} height={24} />, name: "Vue" },
+  { component: <Jest width={24} height={24} />, name: "Jest" },
+  { component: <Playwright width={24} height={24} />, name: "Playwright" },
+  { component: <Laravel width={24} height={24} />, name: "Laravel" },
+  { component: <Expressjs width={24} height={24} />, name: "Express.js" },
+  { component: <PostgreSQL width={24} height={24} />, name: "PostgreSQL" },
+  { component: <MySQL width={24} height={24} />, name: "MySQL" },
+  { component: <Docker width={24} height={24} />, name: "Docker" },
   { component: <TailwindCSS width={24} height={24} />, name: "TailwindCSS" },
   { component: <Nextjs width={24} height={24} />, name: "Next.js" },
   { component: <Astro width={24} height={24} />, name: "Astro" },
-  { component: <PostgreSQL width={24} height={24} />, name: "PostgreSQL" },
-  { component: <Vercel width={24} height={24} />, name: "Vercel" },
-  { component: <Playwright width={24} height={24} />, name: "Playwright" },
-  { component: <Git width={24} height={24} />, name: "Git" },
-  { component: <Github width={24} height={24} />, name: "Github" },
   { component: <Shadcnui width={24} height={24} />, name: "Shadcn/ui" },
 ];
 
